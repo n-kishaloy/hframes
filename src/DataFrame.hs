40b26d490ac454bc0f46a8a60331b1a0ab065817
Kishaloy Neogi <nkishaloy@yahoo.com>
2021-07-11 14:44:47 +0100

    Version 0.0.1

--- src/DataFrame.hs
+++ src/DataFrame.hs
@@ -1,6 +1,65 @@
+{-# LANGUAGE ExistentialQuantification, TemplateHaskell, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE Strict, OverloadedLists, OverloadedStrings #-}
+
+
 module DataFrame
-    ( someFunc
-    ) where
+( someFunc
+, DataFrame (..), HasRecords (..), HasIndex (..), HasHeader (..)
+
+
+) where
+
+
+import Data.Text (Text)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
+
+import Data.Hashable ( Hashable )
+
+import Control.Lens ( makeFields )
+-- import Control.Lens.TH
+
+import Data.Time (Day, UTCTime)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+data DF =
+  DFMInt    (V.Vector (Maybe Int))      | DFInt     (U.Vector Int)      |
+  DFMDouble (V.Vector (Maybe Double))   | DFDouble  (U.Vector Double)   |
+  DFMText   (V.Vector (Maybe Text))     | DFText    (V.Vector Text)     |
+  DFMUTC    (V.Vector (Maybe UTCTime))  | DFUTC     (V.Vector UTCTime)  |
+  DFMDay    (V.Vector (Maybe Day))      | DFDay     (V.Vector Day)      |
+  DFMBool   (V.Vector (Maybe Bool))     | DFBool    (U.Vector Bool)
+  deriving (Show)
+
+data DFIndex =
+  DFIdInt   (U.Vector Int)      | 
+  DFIdText  (V.Vector Text)     | 
+  DFIdDay   (V.Vector Day)      |
+  DFIdTime  (V.Vector UTCTime)
+  deriving (Show)
+
+data DataFrame = DataFrame
+  { dataFrameRecords    ::  V.Vector DF
+  , dataFrameIndex      ::  Maybe DFIndex
+  , dataFrameHeader     ::  Maybe DFIndex
+
+  } deriving (Show) 
+
+makeFields ''DataFrame
+
+toJSON :: DataFrame -> Text 
+toJSON df = undefined 
+
+fromJSON :: Text -> DataFrame 
+fromJSON sr = undefined 
+
+jsonNormalize :: Text -> V.Vector Text -> V.Vector Text -> DataFrame
+jsonNormalize dat recPath meta = undefined
+
+headerToText :: DataFrame -> DataFrame
+headerToText df = undefined 
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,30 @@
+
+
+module Spec where
+
+
+import Data.List (foldl')
+import DataFrame
+
+
+qTest :: Show a => a -> [Bool] -> IO ()
+
+qTest nam x = putStrLn $ lJus 30 '.' nam ++" => Tests :"++ rJus 3 ' ' (p+f) ++fl f where
+  (p,f)=foldl' (\(u,v) b -> if b then (u+1,v) else (u,v+1)) (0,0) x :: (Int,Int)
+  fl 0 = " => Ok"
+  fl z = " => +++++ << FAILED : " ++ show z ++ " >> +++++" 
+  lJus n c xr = st ++ replicate (n - length st) c where st = show xr
+  rJus n c xr = replicate (n - length st) c ++ st where st = show xr 
+
+infix 3 `qCheck`
+qCheck :: Show a => a -> Bool -> IO ()
+qCheck nam False = putStrLn $ "*** Error *** : " ++ show nam ++ "\n"
+qCheck _ _ = putStr ""
+
+
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = 
+  putStrLn "Test suite not yet implemented"
+
