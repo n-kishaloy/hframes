0e11a69a267842437cc1db7ab35bce7a6f177fb6
Kishaloy Neogi <nkishaloy@yahoo.com>
2021-07-23 02:00:46 +0100

    Version 0.1.0.0

--- src/DataFrame.hs
+++ src/DataFrame.hs
@@ -18,17 +18,17 @@ You may see the github repository at <https://github.com/n-kishaloy/hframes>
 
 
 module DataFrame
-( 
-  
-  
+(
+
+
 -- *How to use this library
 -- |Add @hframes@ to build-depends and @import DataFrames@
 
 -- |
 
 -- *Documentation
-  DataFrame (..), HasRecords (..), HasKey (..), HasIndex (..) -- , HasIndex (..)
-, getData, getRecord, toHashMap, fromHashMap, toJSON, fromJSON
+  DataFrame (..), HasRecords (..), HasIndex (..) -- , HasIndex (..)
+, getData, getRecord, toJSON, fromJSON
 , toCSV, fromCSV, info, jsonNormalize, headerToText
 
 
@@ -43,7 +43,8 @@ import qualified Data.HashSet as Hs
 
 import Data.Hashable ( Hashable )
 
-import Control.Lens ( makeFields )
+import Control.Lens
+import Data.Function ((&))
 -- import Control.Lens.TH
 
 import Data.Time (Day, UTCTime)
@@ -51,7 +52,7 @@ import Data.Time (Day, UTCTime)
 {-|@DF = Vector of each Datatype supported by DataFrames@
 
 -}
-data DF =
+data DFVec =
   DFMInt    (V.Vector (Maybe Int))      | DFInt     (U.Vector Int)      |
   DFMDouble (V.Vector (Maybe Double))   | DFDouble  (U.Vector Double)   |
   DFMText   (V.Vector (Maybe Text))     | DFText    (V.Vector Text)     |
@@ -62,145 +63,167 @@ data DF =
   DFMBool   (V.Vector (Maybe Bool))     | DFBool    (U.Vector Bool)
   deriving  (Show)
 
-data RData =
-  RDataMInt     (Maybe Int)             | RDataInt     Int             |
-  RDataMDouble  (Maybe Double)          | RDataDouble  Double          |
-  RDataMText    (Maybe Text)            | RDataText    Text            |
-  RDataMUTC     (Maybe UTCTime)         | RDataUTC     UTCTime         |
-  RDataMDay     (Maybe Day)             | RDataDay     Day             |
-  RDataMCat     (Maybe (V.Vector Text)) | RDataCat     (V.Vector Text) |
-  RDataMBool    (Maybe Bool)            | RDataBool    Bool
+data DType =
+  DTypeMInt     (Maybe Int)             | DTypeInt     Int             |
+  DTypeMDouble  (Maybe Double)          | DTypeDouble  Double          |
+  DTypeMText    (Maybe Text)            | DTypeText    Text            |
+  DTypeMUTC     (Maybe UTCTime)         | DTypeUTC     UTCTime         |
+  DTypeMDay     (Maybe Day)             | DTypeDay     Day             |
+  DTypeMCat     (Maybe (V.Vector Text)) | DTypeCat     (V.Vector Text) |
+  DTypeMBool    (Maybe Bool)            | DTypeBool    Bool
   deriving      (Show)
 
-type Record a = H.HashMap a RData
+type Record a = H.HashMap a DType
 
-data DFIndex b = Hashable b =>
-  DFKeyInt    (H.HashMap Int (V.Vector b))      |
-  DFKeyText   (H.HashMap Text (V.Vector b))     |
-  DFKeyDay    (H.HashMap Day (V.Vector b))      |
-  DFKeyTime   (H.HashMap UTCTime (V.Vector b))  |
-  DFKeyBool   (H.HashMap Bool (V.Vector b))
+data DFIndex = 
+  DFKeyInt    (H.HashMap Int (V.Vector Int))      |
+  DFKeyText   (H.HashMap Text (V.Vector Int))     |
+  DFKeyDay    (H.HashMap Day (V.Vector Int))      |
+  DFKeyTime   (H.HashMap UTCTime (V.Vector Int))  |
+  DFKeyBool   (H.HashMap Bool (V.Vector Int))
 
-data DataFrame a b = (Hashable a, Hashable b) => DataFrame
-  { dataFrameRecords    ::  H.HashMap a DF
-  , dataFrameKey        ::  Maybe (H.HashMap b Int)
-  , dataFrameIndex      ::  H.HashMap a (DFIndex b)
+data DataFrame a = Hashable a => DataFrame
+  { dataFrameRecords    ::  H.HashMap a DFVec
+  , dataFrameIndex      ::  H.HashMap a DFIndex
   }
 
 makeFields ''DataFrame
 
-instance Show (DataFrame a b) where
+instance Show (DataFrame a) where
   show x = undefined
 
-getData :: b -> a -> DataFrame a b -> Maybe RData
-getData ky fd = undefined
+idxDFToDType :: DFVec -> Int -> Maybe DType
+idxDFToDType (DFMInt v) ix    = (v V.!? ix) >>= Just . DTypeMInt
+idxDFToDType (DFInt v) ix     = (v U.!? ix) >>= Just . DTypeInt
+idxDFToDType (DFMDouble v) ix = (v V.!? ix) >>= Just . DTypeMDouble
+idxDFToDType (DFDouble v) ix  = (v U.!? ix) >>= Just . DTypeDouble
+idxDFToDType (DFMText v) ix   = (v V.!? ix) >>= Just . DTypeMText
+idxDFToDType (DFText v) ix    = (v V.!? ix) >>= Just . DTypeText
+idxDFToDType (DFMUTC v) ix    = (v V.!? ix) >>= Just . DTypeMUTC
+idxDFToDType (DFUTC v) ix     = (v V.!? ix) >>= Just . DTypeUTC
+idxDFToDType (DFMDay v) ix    = (v V.!? ix) >>= Just . DTypeMDay
+idxDFToDType (DFDay v) ix     = (v V.!? ix) >>= Just . DTypeDay
+idxDFToDType (DFMCat v) ix    = (v V.!? ix) >>= Just . DTypeMCat
+idxDFToDType (DFCat v) ix     = (v V.!? ix) >>= Just . DTypeCat
+idxDFToDType (DFMBool v) ix   = (v V.!? ix) >>= Just . DTypeMBool
+idxDFToDType (DFBool v) ix    = (v U.!? ix) >>= Just . DTypeBool
 
-getRecord :: b -> DataFrame a b -> Maybe (Record a)
-getRecord ky = undefined
+unsafeIdxDFToDType :: DFVec -> Int -> DType
+unsafeIdxDFToDType (DFMInt v) ix    = DTypeMInt     (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFInt v) ix     = DTypeInt      (U.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMDouble v) ix = DTypeMDouble  (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFDouble v) ix  = DTypeDouble   (U.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMText v) ix   = DTypeMText    (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFText v) ix    = DTypeText     (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMUTC v) ix    = DTypeMUTC     (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFUTC v) ix     = DTypeUTC      (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMDay v) ix    = DTypeMDay     (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFDay v) ix     = DTypeDay      (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMCat v) ix    = DTypeMCat     (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFCat v) ix     = DTypeCat      (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFMBool v) ix   = DTypeMBool    (V.unsafeIndex v ix) 
+unsafeIdxDFToDType (DFBool v) ix    = DTypeBool     (U.unsafeIndex v ix) 
 
-unsafeGetData :: b -> a -> DataFrame a b -> RData
-unsafeGetData ky fd = undefined
+-- |Get DType from key and field. In case key does not exist  
+getData :: DataFrame a -> Int -> a -> Maybe DType
+getData df ky fd = undefined -- p V.! z where
+  -- q = H.lookup fd (df ^. records) 
+  -- p = (df ^. key) >>= H.lookup ky
 
-unsafeGetRecord :: b -> DataFrame a b -> Record a
-unsafeGetRecord ky = undefined
+getRecord :: DataFrame a -> Int -> Maybe (Record a)
+getRecord df ky = undefined
 
-toHashMap :: DataFrame a b -> H.HashMap b (Record a)
-toHashMap df = undefined
+unsafeGetData :: DataFrame a -> Int -> a -> DType
+unsafeGetData df ky fd = undefined
 
-fromHashMap :: H.HashMap b (Record a) -> DataFrame a b
-fromHashMap hs = undefined
+unsafeGetRecord :: DataFrame a -> Int -> Record a
+unsafeGetRecord df ky = undefined
 
-toJSON :: DataFrame a b -> Text
+toJSON :: DataFrame a -> Text
 toJSON df = undefined
 
-fromJSON :: Text -> DataFrame a b
+fromJSON :: Text -> DataFrame a
 fromJSON sr = undefined
 
-toCSV :: DataFrame a b -> Text
+toCSV :: DataFrame a -> Text
 toCSV df = undefined
 
-fromCSV :: Text -> DataFrame a b
+fromCSV :: Text -> DataFrame a
 fromCSV sr = undefined
 
-info :: DataFrame a b -> Text
+info :: DataFrame a -> Text
 info df = undefined
 
-vecToDF :: V.Vector RData -> Maybe DF
-vecToDF rd = case rd V.! 0 of
-  RDataInt _ -> V.mapM f rd >>= (Just . DFInt . U.convert) where
-    f (RDataInt x) = Just x
+vecToDFVec :: V.Vector DType -> Maybe DFVec
+vecToDFVec rd = case rd V.! 0 of
+  DTypeInt _ -> V.mapM f rd >>= (Just . DFInt . U.convert) where
+    f (DTypeInt x) = Just x
     f _ = Nothing
 
-  RDataMInt _ -> V.mapM f rd >>= (Just . DFMInt) where
-    f (RDataMInt x) = Just x
+  DTypeMInt _ -> V.mapM f rd >>= (Just . DFMInt) where
+    f (DTypeMInt x) = Just x
     f _ = Nothing
 
   _ -> Nothing
 
-dfToVec :: DF -> V.Vector RData
+dfToVec :: DFVec -> V.Vector DType
 dfToVec df = undefined
 
-addMaybeDF :: DF -> DF
-addMaybeDF df = undefined 
+addMaybeDFVec :: DFVec -> DFVec
+addMaybeDFVec df = undefined
 
-removeMaybeDF :: DF -> Maybe DF 
-removeMaybeDF df = undefined
+removeMaybeDFVec :: DFVec -> Maybe DFVec
+removeMaybeDFVec df = undefined
 
-unsafeRemoveMaybeDF :: DF -> DF 
-unsafeRemoveMaybeDF df = undefined
+unsafeRemoveMaybeDFVec :: DFVec -> DFVec
+unsafeRemoveMaybeDFVec df = undefined
 
-addMaybeRData :: RData -> RData
-addMaybeRData rd = undefined 
+addMaybeDType :: DType -> DType
+addMaybeDType rd = undefined
 
-removeMaybeRData :: RData -> Maybe RData 
-removeMaybeRData rd = undefined
+removeMaybeDType :: DType -> Maybe DType
+removeMaybeDType rd = undefined
 
-unsafeRemoveMaybeRData :: RData -> RData 
-unsafeRemoveMaybeRData rd = undefined
+unsafeRemoveMaybeDType :: DType -> DType
+unsafeRemoveMaybeDType rd = undefined
 
-removeMaybeDataFrame :: V.Vector a -> DataFrame a b -> Maybe (DataFrame a b)
-removeMaybeDataFrame vc df = undefined 
+removeMaybeDataFrame :: V.Vector a -> DataFrame a -> Maybe (DataFrame a)
+removeMaybeDataFrame vc df = undefined
 
-jsonNormalize :: Text -> V.Vector Text -> V.Vector Text -> DataFrame a b
+jsonNormalize :: Text -> V.Vector Text -> V.Vector Text -> DataFrame a
 jsonNormalize dat recPath meta = undefined
 
-headerToText :: DataFrame a b -> DataFrame a b
+headerToText :: DataFrame a -> DataFrame a
 headerToText df = undefined
 
-idxVecToDF :: U.Vector Int -> DataFrame a b -> DataFrame a b
+idxVecToDF :: U.Vector Int -> DataFrame a -> DataFrame a
 idxVecToDF id df = undefined
 
-keyVecToDF :: V.Vector b -> DataFrame a b -> Maybe (DataFrame a b)
-keyVecToDF ky df = undefined
-
-unsafeKeyVecToDF :: V.Vector b -> DataFrame a b -> DataFrame a b
-unsafeKeyVecToDF ky df = undefined
-
-dfFilter :: V.Vector a -> (V.Vector RData -> Bool) -> DataFrame a b -> DataFrame a b
+dfFilter :: V.Vector a -> (V.Vector DType -> Bool) -> DataFrame a -> DataFrame a
 dfFilter vc fn df = undefined
 
-arrange :: Ord c => V.Vector a -> (V.Vector RData -> c) -> DataFrame a b -> DataFrame a b
+arrange :: Ord c => V.Vector a -> (V.Vector DType -> c) -> DataFrame a -> DataFrame a
 arrange vc fn df = undefined
 
-select :: Hs.HashSet a -> DataFrame a b -> Maybe (DataFrame a b)
+select :: Hs.HashSet a -> DataFrame a -> Maybe (DataFrame a)
 select fl df = undefined
 
-unsafeSelect :: Hs.HashSet a -> DataFrame a b -> DataFrame a b
+unsafeSelect :: Hs.HashSet a -> DataFrame a -> DataFrame a
 unsafeSelect fl df = undefined
 
-conditionSelect :: (a -> Bool) -> DataFrame a b -> DataFrame a b
+conditionSelect :: (a -> Bool) -> DataFrame a -> DataFrame a
 conditionSelect fn df = undefined
 
-rename :: a -> a -> DataFrame a b -> Maybe (DataFrame a b)
+rename :: a -> a -> DataFrame a -> Maybe (DataFrame a)
 rename n0 n1 df = undefined
 
-unsafeRename :: a -> a -> DataFrame a b -> DataFrame a b
+unsafeRename :: a -> a -> DataFrame a -> DataFrame a
 unsafeRename n0 n1 df = undefined
 
-mutate :: H.HashMap a (V.Vector a, V.Vector RData -> RData) -> DataFrame a b -> DataFrame a b
+mutate :: H.HashMap a (V.Vector a, V.Vector DType -> DType) -> DataFrame a -> DataFrame a
 mutate xf df = undefined
 
-transmute :: H.HashMap a (V.Vector a, V.Vector RData -> RData) -> DataFrame a b -> DataFrame a b
+transmute :: H.HashMap a (V.Vector a, V.Vector DType -> DType) -> DataFrame a -> DataFrame a
 transmute xf df = undefined
 
 
